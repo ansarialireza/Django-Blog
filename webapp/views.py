2f62e439cae88a3ae76c9793fc08f35609b28794
alireza <58270983+anssboy@users.noreply.github.com>
2023-04-21 14:39:53 +0330

    add jinja template

--- webapp/views.py
+++ webapp/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse,JsonResponse
+from django.http import HttpResponse, JsonResponse
+
 
 def home(request):
-    return render(request,'website/index.html')
+    return render(request, 'website/home.html')
+
 
 def about(request):
-    return render(request,'website/about.html')
+    return render(request, 'website/about.html')
+
+
 def contact(request):
-    return render(request,'website/contact.html')
+    return render(request, 'website/contact.html')
