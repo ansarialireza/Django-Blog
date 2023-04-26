26e9e9e500f9c0867f8ff5e096f0486aace80422
alireza <58270983+anssboy@users.noreply.github.com>
2023-04-26 10:16:25 +0330

    add venv to git ignore

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from webapp.views import *
 from django.conf.urls.static import static
 from django.conf import settings
+from webapp.views import *
 
 urlpatterns = [
     # url adress  views
