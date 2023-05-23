fc95d3419d84604b776d4adde5c7d19727088534
alireza <ansari09165630639@gmail.com>
2023-05-23 21:45:40 +0430

    last

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,15 @@ from django.db import models
 
 
 class post(models.Model):
+    # image
+    # author
+    # tag
+    # category
+
     title = models.CharField(max_length=255)
     content = models.TextField()
+    counted_views = models.IntegerField(default=0)  # default 0
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
