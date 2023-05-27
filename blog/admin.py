b61541bb98eaca3b1e830ceba552b03fa0388d02
alireza <ansari09165630639@gmail.com>
2023-05-27 17:46:45 +0430

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from blog.models import post
 # Register your models here.
+
+
+# @admin.register(post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'counted_views', 'status',
+                    'published_date', 'created_date')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+
+
+admin.site.register(post, PostAdmin)
