474c1fe9fee8e74339e50958076d4cc77e0961e9
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-10-03 09:13:13 +0330

    Add custom template tags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,32 @@
+from django import template
+from blog.models import Post,Category
+register = template.Library()
+
+@register.simple_tag(name='totalposts')
+def function():
+    posts=Post.objects.filter(status=1).count()
+    return posts
+
+@register.simple_tag(name='posts')
+def function():
+    posts=Post.objects.filter(status=1)
+    return posts
+
+@register.filter
+def snippet(value,arg=20):
+    return value[:20]+". . ."
+
+@register.inclusion_tag('blog/blog-popular-posts.html')
+def popularposts():
+    posts=Post.objects.filter(status=1).order_by('published_date')[:3]
+    return {'posts': posts}
+
+@register.inclusion_tag('blog/blog-post-categories.html')
+def postcategories():
+    posts=Post.objects.filter(status=1)
+    categories=Category.objects.all()
+    cat_dict={}
+    for name in categories:
+        cat_dict[name]=posts.filter(category=name).count()
+    return{'categories':cat_dict}
+
--- blog/views.py
+++ blog/views.py
@@ -2,42 +2,42 @@ from django.shortcuts import render
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from django.http import HttpResponse
 
-def blog_view(request):
+
+def blog_view(request,**kwargs):
     current_time = timezone.now()
-    published_posts = Post.objects.filter(status=True, published_date__lte=current_time).order_by('-published_date')
-    context = {'posts': published_posts}
+    posts = Post.objects.filter(status=True, published_date__lte=current_time).order_by('-published_date')
+    if kwargs.get('cat_name'):
+        posts=posts.filter(category__name=kwargs['cat_name'])
+    if kwargs.get('username'):
+        posts = posts.filter(author__username=kwargs['username'])
+    context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
-
-from django.shortcuts import render, get_object_or_404
-from django.utils import timezone
-from .models import Post
 
 def blog_single(request, pid):
     # Get the current time
     current_time = timezone.now()
-
-    # Retrieve the current post and make sure it's published
     post = get_object_or_404(Post, pk=pid, status=True, published_date__lte=current_time)
-
-    # Get all published posts ordered by published date
     all_posts = Post.objects.filter(status=True, published_date__lte=current_time).order_by('-published_date')
-
-    # Find the index of the current post in the list
     current_index = list(all_posts).index(post)
-
-    # Determine the next and previous posts based on the current post's position
     next_post = all_posts[current_index - 1] if current_index > 0 else None
     previous_post = all_posts[current_index + 1] if current_index < len(all_posts) - 1 else None
-
-    # Increment the view count of the current post
     post.counted_views += 1
     post.save()
-
-    # Render the 'blog/blog-single.html' template with the post and navigation data
     return render(request, 'blog/blog-single.html', {
         'post': post,
         'previous_post': previous_post,
         'next_post': next_post,
     })
+
+
+def blog_search(request):
+    query = request.GET.get('s')  # Retrieve the 's' parameter from the GET request
+    posts = Post.objects.filter(status=1)
+
+    if query:
+        # Use 'icontains' for case-insensitive search in both title and content
+        posts = posts.filter(title__icontains=query) | posts.filter(content__icontains=query)
+
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
