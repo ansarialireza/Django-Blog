87f36ec9a5e0def17ddfdb27330dad6a23ed1255
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-10-04 09:15:36 +0330

    add status filter to custom template tag

--- webapp/templatetags/main_tags.py
+++ webapp/templatetags/main_tags.py
@@ -4,5 +4,5 @@ register = template.Library()
 
 @register.inclusion_tag('website/index-blog.html')
 def display_latest_posts():
-    posts=Post.objects.order_by('-published_date')[0:6]
+    posts=Post.objects.filter(status=1).order_by('-published_date')[0:6]
     return {'posts':posts}
