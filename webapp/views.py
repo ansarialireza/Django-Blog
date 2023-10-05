37579125a5bf4c4b57f5da687e29208d0663384a
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-10-05 14:16:22 +0330

    update main tas

--- webapp/views.py
+++ webapp/views.py
@@ -1,17 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect,HttpResponse
+from .forms import ContactForm
 
 def home(request):
     return render(request, 'website/index.html')
-
-
 def about(request):
     return render(request, 'website/about.html')
+def elements(request):
+    return render(request, 'website/elements.html')
 
 
 def contact(request):
-    return render(request, 'website/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Save the form data to the database
+            form.save()
+            # Redirect to a thank you page or any other appropriate page
+            return HttpResponse("Form submitted successfully")
 
+    else:
+        form = ContactForm()
 
-def elements(request):
-    return render(request, 'website/elements.html')
+    return render(request, 'website/contact.html', {'form': form})
+
