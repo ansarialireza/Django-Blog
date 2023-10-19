b69c42bb25c1afb19be53a2537411f36057da2e7
alireza <58270983+ansarialireza@users.noreply.github.com>
2023-10-19 18:51:49 +0330

    add registration template

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    
     path('password_reset/', auth_views.PasswordResetView.as_view(form_class=CustomPasswordResetForm), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
